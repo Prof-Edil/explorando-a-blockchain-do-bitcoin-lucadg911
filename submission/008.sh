00fbea9aee083c6ca8d3654d461f1df365729244
Luca DG <lucagontijo911@outlook.com>
2024-11-20 06:20:05 -0300

    added syntax to run rust executable

--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,3 @@
-# Which public key signed input 0 in this tx:
-#   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+#!/bin/bash
+cd rust_programs/target/release
+./rust_programs 008
