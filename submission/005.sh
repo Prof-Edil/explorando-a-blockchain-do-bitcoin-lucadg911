39ac97fb8d3aa5aadddbe6b1e4f8edb49cfffcd3
Luca DG <lucagontijo911@outlook.com>
2024-11-23 16:32:51 -0300

    solved 005

--- submission/005.sh
+++ submission/005.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd ..
-cd rust_programs/target/release
-./rust_programs 005
+XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+DERIVATION="/100"
+USER="-rpcuser=user_240"
+PASSWORD="-rpcpassword=JQs8WLu7N4dR"
+IP="-rpcconnect=84.247.182.145"
+
+DESCRIPTOR=$(bitcoin-cli $IP $USER $PASSWORD getdescriptorinfo "tr($XPUB$DERIVATION)"| jq -r '.descriptor')
+ADDRESS=$(bitcoin-cli $IP $USER $PASSWORD deriveaddresses "$DESCRIPTOR" | jq -r '.[0]')
+
+echo $ADDRESS
