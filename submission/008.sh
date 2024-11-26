fcac89d8f2b424a5dc22435c68619769b526c6d0
Luca DG <lucagontijo911@outlook.com>
2024-11-26 11:39:03 -0300

    solved 008

--- submission/008.sh
+++ submission/008.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd ..
-cd rust_programs/target/release
-./rust_programs 008
+USER="-rpcuser=user_240"
+PASSWORD="-rpcpassword=JQs8WLu7N4dR"
+IP="-rpcconnect=84.247.182.145"
+
+TXID="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+
+TRANSACTION=$(bitcoin-cli $USER $PASSWORD $IP getrawtransaction $TXID true)
+TXINWITNESS=$(echo "$TRANSACTION" | jq -r '.vin[0].txinwitness[2]')
+PUB_KEY1=$(echo "$TXINWITNESS" | cut -c 5-70)
+echo "$PUB_KEY1"
