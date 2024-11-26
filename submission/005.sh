0346ad78f43fd76cb1501d625b515f252cdf8ce7
Luca DG <lucagontijo911@outlook.com>
2024-11-26 08:19:04 -0300

    solved 005

--- submission/005.sh
+++ submission/005.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-DERIVATION="/100"
 USER="-rpcuser=user_240"
 PASSWORD="-rpcpassword=JQs8WLu7N4dR"
 IP="-rpcconnect=84.247.182.145"
+TXID="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
 
-DESCRIPTOR=$(bitcoin-cli $IP $USER $PASSWORD getdescriptorinfo "tr($XPUB$DERIVATION)"| jq -r '.descriptor')
-ADDRESS=$(bitcoin-cli $IP $USER $PASSWORD deriveaddresses "$DESCRIPTOR" | jq -r '.[0]')
+HEX_TRANSACTION=$(bitcoin-cli $USER $PASSWORD $IP getrawtransaction $TXID)
+TRANSACTION=$(bitcoin-cli $USER $PASSWORD $IP decoderawtransaction "$HEX_TRANSACTION")
+PUB_KEYS=$(echo "$TRANSACTION" | jq -r '.vin[] | .txinwitness[1]')
 
-echo $ADDRESS
+PUB_KEYS_ARRAY=($(echo "$PUB_KEYS"))
+
+PUB_KEY1="${PUB_KEYS_ARRAY[0]}"
+PUB_KEY2="${PUB_KEYS_ARRAY[1]}"
+PUB_KEY3="${PUB_KEYS_ARRAY[2]}"
+PUB_KEY4="${PUB_KEYS_ARRAY[3]}"
+
+MULTISIG=$(bitcoin-cli $USER $PASSWORD $IP createmultisig 1 "[\"$PUB_KEY1\", \"$PUB_KEY2\", \"$PUB_KEY3\", \"$PUB_KEY4\"]")
+P2SH_ADDRESS=$(echo "$MULTISIG" | jq -r '.address')
+
+echo "$P2SH_ADDRESS"
